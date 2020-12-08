54024d3b5141132e91328f6fb9117d089bbe52ee
elodiebhs <74989692+elodiebhs@users.noreply.github.com>
2020-12-07 17:51:10 -0800

    push
    puah

---
+++ Projects/Portfolio/calculator.js
@@ -0,0 +1,15 @@
+let runningTotal = O;
+let buffer= "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttomClick(value) {}
+
+function init () {
+document.querySelector('.calc-buttons')
+    .addEventListener('click',function (event)) {
+        
+    }
+
+}
